e4cff5f72f857064be1ce5c52444692ff40ea5f7
Dmitriy Prihodin <d.prihodin816@gmail.com>
2026-04-27 23:25:08 +0300

    feat: add auth templates and base layout

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("core.urls")),
     path("auth/", include("django.contrib.auth.urls")),
-    # path("", include("core.urls")),
-    # path("auth/", include("users.urls")),
+    path("auth/", include("users.urls")),
     # path("habits/", include("habits.urls")),
     # path("analytics/", include("analytics.urls")),
 ]
