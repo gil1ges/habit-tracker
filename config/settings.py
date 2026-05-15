7cfc395d0c6f9be8ba17f1000f601ed5bd836165
Hayden2572 <nikitaborisovv353@gmail.com>
2026-05-15 10:35:00 +0300

    Add SMTP email settings

--- config/settings.py
+++ config/settings.py
@@ -118,8 +118,35 @@ STATIC_ROOT = Path(os.getenv("DJANGO_STATIC_ROOT", BASE_DIR / "staticfiles"))
 MEDIA_URL = os.getenv("DJANGO_MEDIA_URL", "/media/")
 MEDIA_ROOT = Path(os.getenv("DJANGO_MEDIA_ROOT", BASE_DIR / "media"))
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.filebased.EmailBackend",
+)
+
 EMAIL_FILE_PATH = Path(os.getenv("DJANGO_EMAIL_FILE_PATH", BASE_DIR / "sent_emails"))
+
+EMAIL_HOST = os.getenv("EMAIL_HOST", "")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False").lower() in (
+    "true",
+    "1",
+    "yes",
+    "on",
+)
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False").lower() in (
+    "true",
+    "1",
+    "yes",
+    "on",
+)
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+
+DEFAULT_FROM_EMAIL = os.getenv(
+    "DEFAULT_FROM_EMAIL",
+    EMAIL_HOST_USER or "webmaster@localhost",
+)
+SERVER_EMAIL = os.getenv("SERVER_EMAIL", DEFAULT_FROM_EMAIL)
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "habit_list"
